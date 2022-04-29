b4559e81ebf5b745ba559108f3b388229cc5d9b9
BarbosaXBarbosa <asobrab123@hotmail.com>
2022-04-29 13:55:33 -0300

    Construindo estrutura, estilo e funcionalidade do jogo.

---
+++ script.js
@@ -0,0 +1,43 @@
+let order = [];
+let clickedOrder = [];
+let score = 0;
+
+// 0 = verde
+// 1 = vermelho
+// 2 = amarelo
+// 3 = azul
+
+const blue = document.querySelector('.blue');
+const red = document.querySelector('.red');
+const yellow = document.querySelector('.yellow');
+const green = document.querySelector('.green');
+
+let shuffleOrder = () => {
+    let colorOrder = Math.floor(Math.random);
+    order[order.lenght] = colorOrder;
+    clickedOrder = [];
+    
+    for(let i in order) {
+        let elementColor = createColorElement(order[i])
+        lightColor(elementColor, Number(i) + 1)
+    }
+}
+
+let lightColor = (element, number) => {
+    number = number * 500;
+    setTimeout(() => {
+        element.classList.add('selected');
+    }, number-250);
+    setTimeout(() => {
+        element.classList.removed('selected')
+    });
+}
+
+let checkOrder = () => {
+    for(let i in clickedOrder) {
+        if(clickedOrder [i] != order[i]) {
+            lose();
+            break;
+        }
+    }
+}
